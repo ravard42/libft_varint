5f4adad51565a7a78de29a49f22bfc3ce373d4e8
ravard <avardrobin@hotmail.com>
2020-01-10 23:47:45 +0100

    ft_is_decimal fixed

--- src/ft_is_decimal.c
+++ src/ft_is_decimal.c
@@ -7,7 +7,7 @@ bool		ft_is_decimal(char *str)
 	if (!str || !str[0])
 		return (false);
 	i = -1;
-	while (str[++i] && str[i] >= '0' && str[i] <= 9)
+	while (str[++i] && str[i] >= '0' && str[i] <= '9')
 		;
 	if (str[i])
 		return (false);
