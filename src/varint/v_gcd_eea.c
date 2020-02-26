b7708f712bb2feb5effb4c90eb453006dff58744
ravard <avardrobin@hotmail.com>
2020-02-26 22:56:04 +0100

    v_gcd_eea_inv.c -> v_gcd_eea.c and v_crt.c -> v_inv_crt.c

--- src/varint/v_gcd_eea.c
+++ src/varint/v_gcd_eea.c
@@ -47,7 +47,7 @@ t_varint		v_gcd(t_varint a, t_varint b, bool check)
 ** we know that final resultat (coef_r0, coef_r0 + 1) can be contained in the same varint size that inputs
 ** however when processing v_mul algo in eea it could overflow in the way we handle carry (32-bit * 32-bit in 64-bit)
 ** (cf v_mul algo -> u64 = (uint64_t *)((uint32_t *)ret->x + i + j);)
-** let alen32, blen32 and maxlen32 that represent the number of uint32_t chunck necessary to contained respectively a b and V_MAX_LEN chuncks 
+** let alen32, blen32 and maxlen32 that represent the number of uint32_t chunk necessary to contained respectively a, b and V_MAX_LEN chunks 
 **	we want max32 >= alen32 + blen32
 */
 
@@ -74,6 +74,22 @@ static void		init_coef(t_varint *coef_r0, t_varint *coef_r1)
 	coef_r1[1] = g_v[1];
 }
 
+static void	process(t_varint *coef_r0, t_varint *coef_r1, t_varint *r)
+{
+	t_varint	tmp[2];
+
+	tmp[0] = v_mod(r[0], r[1], true, false);
+	tmp[1] = v_div(v_sub(r[0], tmp[0], false), r[1], false);
+	r[0] = r[1];
+	r[1] = tmp[0];
+	tmp[0] = v_sub(coef_r0[0], v_mul(tmp[1], coef_r1[0], false), false);
+	tmp[1] = v_sub(coef_r0[1], v_mul(tmp[1], coef_r1[1], false), false);
+	coef_r0[0] = coef_r1[0];
+	coef_r0[1] = coef_r1[1];
+	coef_r1[0] = tmp[0];
+	coef_r1[1] = tmp[1];
+}
+
 /*
 ** r[2]			: deux derniers restes
 ** coef_r1[2]	: coef alpha et beta de r[1]
@@ -84,7 +100,6 @@ void			v_eea(t_varint *coef_r0, t_varint a, t_varint b, bool check)
 	int8_t	sign[3];
 	t_varint	coef_r1[2];
 	t_varint	r[2];
-	t_varint	tmp[2];
 
 	if (check && !v_check(&a, &b, NULL, "eea"))
 		return ;
@@ -93,42 +108,13 @@ void			v_eea(t_varint *coef_r0, t_varint a, t_varint b, bool check)
 	r[0] = a;
 	r[1] = b;
 	while (!is_g_v(0, r + 1))
-	{
-		tmp[0] = v_mod(r[0], r[1], true, false);
-		tmp[1] = v_div(v_sub(r[0], tmp[0], false), r[1], false);
-		r[0] = r[1];
-		r[1] = tmp[0];
-		tmp[0] = v_sub(coef_r0[0], v_mul(tmp[1], coef_r1[0], false), false);
-		tmp[1] = v_sub(coef_r0[1], v_mul(tmp[1], coef_r1[1], false), false);
-		coef_r0[0] = coef_r1[0];
-		coef_r0[1] = coef_r1[1];
-		coef_r1[0] = tmp[0];
-		coef_r1[1] = tmp[1];
-	}
+		process(coef_r0, coef_r1, r);
 	coef_r0[0].sign *= (sign[0] != a.sign) ? -1 : 1;
 	(coef_r0[1]).sign *= (sign[1] != a.sign) ? -1 : 1;
 	if (sign[2])
 	{
-		tmp[0] = coef_r0[0];
+		r[0] = coef_r0[0];
 		coef_r0[0] = coef_r0[1];
-		coef_r0[1] = tmp[0];
+		coef_r0[1] = r[0];
 	}	
-}
-
-t_varint		v_inv(t_varint v, t_varint mod, bool check)
-{
-	t_varint	tmp[2];
-	t_varint	gcd;
-
-	if (check && !v_check(&v, NULL, &mod, "inv"))
-		return (g_v[3]);
-	gcd = v_gcd(v, mod, false);
-	if (!is_g_v(1, &gcd)
-		&& ft_dprintf(2, "%s%s%s", KRED, V_INV_MOD_ERR, KNRM))
-		return (g_v[3]);
-	v_eea(tmp, v, mod, false);
-//	if (v_cmp(&v, "-gt", &mod, false))
-//		return (tmp[0]);
-//	return (tmp[1]);
-	return (tmp[0]);
 }
