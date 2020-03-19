7eee9db0d4c2b8589d725972599d3f15808946ab
ravard <avardrobin@hotmail.com>
2020-03-19 09:48:29 +0100

    new option gestion in ft_puthex and ft_putrhex

--- src/ft_puthex.c
+++ src/ft_puthex.c
@@ -23,34 +23,52 @@ static void		put_hex_digit(uint8_t x, bool capitalize)
 		ft_putchar(a + x - 10);
 }
 
-void			ft_puthex(void *p, int64_t len, bool capitalize, bool newline)
+
+static void		heading_zero(uint8_t first_byte, uint8_t opts)
+{
+	uint8_t tmp;
+
+	tmp = first_byte >> 4;
+	if (tmp != 0 || (opts & 4))
+		put_hex_digit(tmp, opts & 2);
+	put_hex_digit(first_byte & 0x0F, opts & 2);
+}
+
+/*
+** options: heading_0 capitalize newline
+**					100			010		001
+*/
+
+void			ft_puthex(void *p, int64_t len, uint8_t opts)
 {
 	uint8_t		*p_8;
 	int64_t		i;
 
 	p_8 = (uint8_t *)p;
-	i = -1;
+	heading_zero(p_8[0], opts);
+	i = 0;
 	while (++i < len)
 	{
-		put_hex_digit(p_8[i] >> 4, capitalize);
-		put_hex_digit(p_8[i] & 0x0F, capitalize);
+		put_hex_digit(p_8[i] >> 4, opts & 2);
+		put_hex_digit(p_8[i] & 0x0F, opts & 2);
 	}
-	if (newline)
+	if (opts & 1)
 		ft_putchar('\n');
 }
 
-void			ft_putrhex(void *p, int64_t len, bool capitalize, bool newline)
+void			ft_putrhex(void *p, int64_t len, uint8_t opts)
 {
 	uint8_t		*p_8;
 	int64_t		i;
 
 	p_8 = (uint8_t *)p;
-	i = len;
+	heading_zero(p_8[len - 1], opts);
+	i = len - 1;
 	while (--i >= 0)
 	{
-		put_hex_digit(p_8[i] >> 4, capitalize);
-		put_hex_digit(p_8[i] & 0x0F, capitalize);
+		put_hex_digit(p_8[i] >> 4, opts & 2);
+		put_hex_digit(p_8[i] & 0x0F, opts & 2);
 	}
-	if (newline)
+	if (opts & 1)
 		ft_putchar('\n');
 }
